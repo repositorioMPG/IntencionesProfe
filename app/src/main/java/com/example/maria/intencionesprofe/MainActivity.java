4e52721ce64a85600eee091a13e2c2a466303463
maria <profesor.mpg@gmail.com>
2019-10-18 08:47:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/maria/intencionesprofe/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.maria.intencionesprofe;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void pgWeb(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.cifpcarballeira.es/"));
+        startActivity(intent);
+    }
+
+    public void llamadaTelefono(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL,
+                Uri.parse("tel:962849347"));
+        startActivity(intent);
+    }
+
+    public void googleMaps(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("geo:42.331933,-7.875000"));
+        startActivity(intent);
+    }
+
+    public void tomarFoto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(intent);
+    }
+
+    public void mandarCorreo(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "asunto");
+        intent.putExtra(Intent.EXTRA_TEXT, "texto del correo");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"profesor.mpg@gmail.com"});
+        startActivity(intent);
+    }
+
+
+}
